7023da6267ad536686b58f7f521e7bc539b14d2c
Iryna Kuziv <106926689+IraAmber@users.noreply.github.com>
2023-08-10 20:23:02 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./app.css";
+import Accordion from "./Components/Accordion";
+import { useSpring, animated } from "react-spring";
+
+function App() {
+  //title animation
+  const titleAnimation = useSpring({
+    from: {
+      transform: "translateY(-30px)"
+    },
+    to: [{ transform: "translateY(15px)" }],
+    config: { mass: 3, tension: 500, friction: 25 }
+  });
+
+  return (
+    <div className="main">
+      <animated.h1 style={titleAnimation}>React Accordion</animated.h1>
+      <div className="accordion">
+        <Accordion
+          title="Item 1 - Lorem ipsum dolor sit amet"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        />
+        <Accordion
+          title="Item 2 - Lorem ipsum dolor sit amet"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        />
+        <Accordion
+          title="Item 3 - Lorem ipsum dolor sit amet"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        />
+        <Accordion
+          title="Item 4 - Lorem ipsum dolor sit amet"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
